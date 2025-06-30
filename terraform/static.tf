72be04d14222e58fcdc76df4b91cec82983583a3
Edgar Delgado <edgar.jose.fernando.delgado@gmail.com>
2025-06-30 06:42:12 -0500

    adding static bucket assets

---
+++ terraform/static.tf
@@ -0,0 +1,39 @@
+resource "google_storage_bucket" "static_site" {
+  name     = "${var.environment}-nogales-assets"
+  location = "US"
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+  uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket_iam_member" "static_site_access" {
+  bucket = google_storage_bucket.static_site.name
+  role   = "roles/storage.objectViewer"
+  member = "allUsers"
+}
+
+resource "google_compute_managed_ssl_certificate" "static_ssl" {
+  name    = "${var.environment}-static-cert"
+  managed {
+    domains = ["apps.solvista.me"]
+  }
+}
+
+resource "google_compute_backend_bucket" "static_backend" {
+  name        = "${var.environment}-static-backend-bucket"
+  bucket_name = google_storage_bucket.static_site.name
+  enable_cdn  = true
+}
+
+resource "google_compute_url_map" "static_map" {
+  name            = "${var.environment}-static-url-map"
+  default_service = google_compute_backend_bucket.static_backend.id
+}
+
+resource "google_compute_target_https_proxy" "static_proxy" {
+  name             = "${var.environment}-https-proxy"
+  ssl_certificates = [google_compute_managed_ssl_certificate.static_ssl.id]
+  url_map          = google_compute_url_map.static_map.id
+}
