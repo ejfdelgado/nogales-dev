d36aa7716bdf12f97a251cc6d18de29586b47547
Edgar Delgado <edgar.jose.fernando.delgado@gmail.com>
2025-07-07 06:16:58 -0500

    use reporitory only for production

--- terraform/registry.tf
+++ terraform/registry.tf
@@ -1,4 +1,7 @@
+# us-central1-docker.pkg.dev/local-volt-431316-m2/nogales/image-name
+
 resource "google_artifact_registry_repository" "docker_repo" {
+  count = var.environment == "pro" ? 1 : 0
   provider  = google
   location  = "us-central1"
   repository_id = "nogales"
