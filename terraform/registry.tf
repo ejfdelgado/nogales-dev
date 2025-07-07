4fed06ac29df8285425554855d51b97cd58c38d1
Edgar Delgado <edgar.jose.fernando.delgado@gmail.com>
2025-07-07 07:53:50 -0500

    using new registry

--- terraform/registry.tf
+++ terraform/registry.tf
@@ -1,4 +1,19 @@
-# us-central1-docker.pkg.dev/local-volt-431316-m2/nogales/image-name
+
+resource "google_project_service" "artifact_registry" {
+  service = "artifactregistry.googleapis.com"
+}
+
+# gcloud auth configure-docker us-central1-docker.pkg.dev
+
+# us-docker.pkg.dev/ejfexperiments/us.gcr.io/nogales_videocall_front_back:1.18.1
+# us-central1-docker.pkg.dev/local-volt-431316-m2/nogales/nogales_videocall_front_back:1.18.1
+# docker tag us-docker.pkg.dev/ejfexperiments/us.gcr.io/nogales_videocall_front_back:1.18.1 us-central1-docker.pkg.dev/local-volt-431316-m2/nogales/nogales_videocall_front_back:1.18.1
+# docker push us-central1-docker.pkg.dev/local-volt-431316-m2/nogales/nogales_videocall_front_back:1.18.1 && npm run bip
+
+# us-docker.pkg.dev/ejfexperiments/us.gcr.io/nogales_assessment_front_back:
+# us-central1-docker.pkg.dev/local-volt-431316-m2/nogales/nogales_assessment_front_back:
+# docker tag us-docker.pkg.dev/ejfexperiments/us.gcr.io/nogales_assessment_front_back:1.37.2 us-central1-docker.pkg.dev/local-volt-431316-m2/nogales/nogales_assessment_front_back:1.37.2
+# docker push us-central1-docker.pkg.dev/local-volt-431316-m2/nogales/nogales_assessment_front_back:1.37.2 && npm run bip
 
 resource "google_artifact_registry_repository" "docker_repo" {
   count = var.environment == "pro" ? 1 : 0
