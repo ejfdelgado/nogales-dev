5644c81e92f63f3d6114dbe169fd57d5343eeefe
Edgar Delgado <edgar.jose.fernando.delgado@gmail.com>
2026-04-22 08:23:19 -0500

    created the n8n certificate

--- terraform/n8n.tf
+++ terraform/n8n.tf
@@ -1,1 +1,9 @@
 
+resource "google_compute_managed_ssl_certificate" "n8n" {
+  name    = "${var.environment}-n8n-cert"
+  managed {
+    domains = [
+      var.environment == "pro" ? "n8n.solvista.me." : "n8n-stg.solvista.me"
+      ]
+  }
+}
