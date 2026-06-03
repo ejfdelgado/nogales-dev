0ccf3ec8e1ab5c024a3e7a3cd9e87e87f9c73890
Edgar Delgado <edgar.jose.fernando.delgado@gmail.com>
2026-06-03 05:33:36 -0500

    no terraform here

--- terraform/firestore.tf
+++
@@ -1,7 +0,0 @@
-resource "google_firestore_database" "database" {
-  project     = var.project_name
-  name        = "${var.environment}-room-intro"
-  # nam5 Estados Unidos, eur3 Europa
-  location_id = "nam5"
-  type        = "FIRESTORE_NATIVE"
-}
--- vpn-terraform/disks.tf
+++
@@ -1,9 +0,0 @@
-
-resource "google_compute_disk" "stateful_disks" {
-  count = 1
-  name  = "${var.environment}-vpn-disk-${count.index}"
-  type  = "pd-standard"
-  zone  = var.zone
-  size  = 5
-}
-
